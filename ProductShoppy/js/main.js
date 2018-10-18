268b82d4e0f1e89d7e7a7b96e52b0b4c64802d33
Dnyanda-Deshpande <41358125+Dnyanda-Deshpande@users.noreply.github.com>
2018-10-18 14:54:53 +0530

    Add files via upload

--- ProductShoppy/js/main.js
+++ ProductShoppy/js/main.js
@@ -1,11 +1,23 @@
-	
-	$(document).ready(function(){
-    $("button").click(function(){
-        $.getJSON("../json/mobileData.json", function(result){
-            $.each(result, function(i, field){
-                $("div").append(field + " ");
-				console.log(result);
-            });
+function loginAdmin() {
+	var username = document.forms["adminLoginForm"]["userName"].value;
+	var password = document.forms["adminLoginForm"]["AdminPassword"].value;
+	if (username == "dnyanda" && password=="123") {
+		console.log("Hello");
+		window.location="adminPage.html";
+	}
+	else
+	{
+		alert("Please Enter Valid Credentials");
+	}
+}
+
+$(document).ready(function(){
+   
+    $.getJSON("../json/data.json", function(result){
+        $.each(result, function(i, field){
+            $("#navCategories").append("<li>"+field.category + "</li> ");
+            console.log(field.category);
         });
     });
+
 });
