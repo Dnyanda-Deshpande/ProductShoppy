41a2f504fb888821f82c610bc0d86c686533d7fe
Dnyanda-Deshpande <41358125+Dnyanda-Deshpande@users.noreply.github.com>
2018-10-21 22:05:25 +0530

    Add files via upload

--- ProductShoppy/js/main.js
+++ ProductShoppy/js/main.js
@@ -11,33 +11,36 @@ function loginAdmin() {
 	}
 }
 
-/*$(document).ready(function(){
-   
-    $.getJSON("../json/data.json", function(result){
-        $.each(result, function(i, field){
-            $("#navCategories").append("<li>"+field.category + "</li> ");
-            console.log(field.category);
+/**********displaying catergories with sub categories****************/
+$(document).ready(function(){
+    $.getJSON("../json/category.json",function(result){
+        $.each(result, function(i,category){
+            var subMenuData='';
+            $.each(category.sub_categories, function(i,sub_categories){
+                subMenuData += "<li><a>"+sub_categories.name+"</a></li>";
+
+            });
+                var mainMenuData ="<li><a>"+category.categoryName+
+                "</a><ul>"+subMenuData+"</ul></li>";
+            $(mainMenuData).appendTo("#navCategories");
         });
     });
 
-});*/
 
 
-$(document).ready(function(){
-    var productData = [];
-    var categoriesSet = new Set(); 
-    $.get("../json/data.json", function(result) {
-        productData = result;
-      (function() {
-            for (var product of productData) {
-              categoriesSet.add(product.category);
-            }
-            var parentListItem = document.getElementById("navCategories");
-            for (var item of categoriesSet) {
-              var childListItem = document.createElement("li");
-              childListItem.innerHTML = item;
-              parentListItem.appendChild(childListItem);
-            }
-          }());
+    /*****getting all data*****************/
+    $.getJSON("../json/data.json",function(data){
+        var productData = '';
+        $.each(data,function(key,value){
+         
+            productData += '<tr><td  style="padding-right: 250px" rowspan="5"><img src= '+value.imageUrl+ ' width="200px" height="350px"></td></tr>';
+            productData += '<tr><td>' + value.name+ '</td></tr>';
+            productData += '<tr><td>' + value.price+ '</td></tr>';
+            productData += '<tr><td> <button  class="btn btn-success"><span class="glyphicon glyphicon-ok"></span> Add to Cart</button></tr>';
+            productData += '<tr><td> <button  class="btn btn-danger"><span class="glyphicon glyphicon-heart"></span> Add To wishList</button></tr>';
+        
         });
+        $("#productDataTable").append(productData);
+    })
+
 });
